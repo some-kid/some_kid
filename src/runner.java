3db4e5297e27cd34316b30cc283bdb326002cab1
some_kid <sergeantcorn@gmail.com>
2011-04-14 17:04:21 -0500

    Hello World!

---
+++ src/runner.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jeremiah Buddenhagen
+ *
+ */
+public class runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
